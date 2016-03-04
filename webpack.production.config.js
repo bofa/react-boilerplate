1a7c799483e2ed2e044a4230ee9e608d4669e254
bofa <prosit86@gmail.com>
2016-03-04 08:57:58 +0100

    Build tool.

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -18,9 +18,15 @@ module.exports = {
   ],
   module: {
     loaders: [{
-      test: /\.jsx?$/,
-      loaders: ['babel'],
-      include: path.join(__dirname, 'src')
-    }]
+            test: /\.jsx?$/,
+            loaders: ['react-hot', 'babel'],
+            include: path.join(__dirname, 'src')
+        },
+        { 
+            test: /\.css$/, 
+            loader: 'css-loader' 
+        
+        },
+    ]
   }
 };
